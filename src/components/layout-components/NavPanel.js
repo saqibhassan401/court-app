41afc2e943e3142a5ce1fe437c41d6e8ab9c6b6d
saqib hassan <saqib.hassan401@gmail.com>
2021-02-10 22:18:55 +0500

    Adding app in github

---
+++ src/components/layout-components/NavPanel.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { SettingOutlined } from '@ant-design/icons';
+import { Drawer, Menu } from 'antd';
+import ThemeConfigurator from './ThemeConfigurator';
+import { connect } from "react-redux";
+import dashboardSettings from "../../assets/icons/Iconly_Bold_Setting.png"
+
+export class NavPanel extends Component {
+	state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+	};
+
+	render() {
+		return (
+      <>
+        <Menu mode="horizontal">
+          <Menu.Item onClick={this.showDrawer}>
+            <img src={dashboardSettings}/>
+          </Menu.Item>
+        </Menu>
+        <Drawer
+          title="Theme Config"
+          placement="right"
+          width={350}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <ThemeConfigurator/>
+        </Drawer>
+      </>
+    );
+	}
+}
+
+const mapStateToProps = ({ theme }) => {
+  const { locale } =  theme;
+  return { locale }
+};
+
+export default connect(mapStateToProps)(NavPanel);
