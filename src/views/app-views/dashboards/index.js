4832da3dd269e1b0c21009454c5702b9ac062c66
saqib hassan <saqib.hassan401@gmail.com>
2021-02-15 12:02:25 +0500

    language changes

--- src/views/app-views/dashboards/index.js
+++ src/views/app-views/dashboards/index.js
@@ -7,9 +7,6 @@ const Dashboards = ({ match }) => {
   <Suspense fallback={<Loading cover="content"/>}>
     <Switch>
       <Route path={`${match.url}/default`} component={lazy(() => import(`./default`))} />
-      <Route path={`${match.url}/analytic`} component={lazy(() => import(`./analytic`))} />
-      <Route path={`${match.url}/sales`} component={lazy(() => import(`./sales`))} />
-      <Redirect from={`${match.url}`} to={`${match.url}/default`} />
     </Switch>
   </Suspense>
 )};
